d183ac1fb0f7ede8c7331eb7ae48c550cca46291
Atha Zaul Ghithrif <atha.zaul24@students.utdi.ac.id>
2026-07-23 16:11:51 +0700

    --

--- CODE/connectdots.gd
+++ CODE/connectdots.gd
@@ -4,13 +4,18 @@ extends Control
 @onready var switch_3: CheckButton = $CenterContainer/Panel/VBoxContainer/switch3
 @onready var switch_4: CheckButton = $CenterContainer/Panel/VBoxContainer/switch4
 
+var all_switch := []
+
 func _ready() -> void:
 	switch_3.button_down.connect(switch3_on)
 	switch_1.button_down.connect(switch1_on)
 	switch_4.button_down.connect(switch4_on)
+	all_switch = [switch_1,switch_2,switch_3,switch_4]
 	
 func _process(delta: float) -> void:
-	pass
+	if switch_2.pressed:
+		if all_switch.all(func(sw): return sw.button_pressed):
+			$Label.text = "all switch are on"
 
 func switch3_on():
 	if switch_1.button_pressed:
