fd99c36b50f3a73c8876dc80ab18587a137a5cba
Atha Zaul Ghithrif <atha.zaul24@students.utdi.ac.id>
2026-07-23 15:23:46 +0700

    Update connectdots.gd

--- CODE/connectdots.gd
+++ CODE/connectdots.gd
@@ -6,6 +6,8 @@ extends Control
 
 func _ready() -> void:
 	switch_3.button_down.connect(switch3_on)
+	switch_1.button_down.connect(switch1_on)
+	switch_4.button_down.connect(switch4_on)
 	
 func _process(delta: float) -> void:
 	pass
@@ -13,3 +15,11 @@ func _process(delta: float) -> void:
 func switch3_on():
 	if switch_1.button_pressed:
 		switch_1.button_pressed = false
+func switch1_on():
+	if switch_2.button_pressed:
+		switch_2.button_pressed = false
+	if switch_4.button_pressed:
+		switch_4.button_pressed = false
+func switch4_on():
+	if switch_2.button_pressed:
+		switch_2.button_pressed = false
